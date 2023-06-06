ce6374cc3d818d9efa84e2f37c106c08f8a4aa85
rocnail23 <jdanielrojas16@gmail.com>
2023-06-06 01:06:24 -0400

    Las commit

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Route,Navigate } from 'react-router-dom'
 export const PrivateRoute = ({component: Component, ...props}) => {
     const {authenticated,loading} = useContext(authContex)
   
-   if(!authenticated && !loading){
+   if(!authenticated){
     return (<Navigate to="/" />)}else{
         return  <Component {...props}/>
     }
